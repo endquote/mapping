b029801cc1c7f6b53d73c6a50b82d01b2364cea5
Josh Santangelo <josh@endquote.com>
2022-04-03 08:19:42 -0700

    more boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getWidth, getHeight, transposeEvent } from "./viewport";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [isMobile, setIsMobile] = useState(window.navigator.maxTouchPoints > 0);
+  const [width, setWidth] = useState(getWidth());
+  const [height, setHeight] = useState(getHeight());
+
+  useEffect(() => {
+    window.addEventListener("resize", resize, false);
+    window.addEventListener("orientationchange", resize, false);
+    window.addEventListener("pointermove", pointermove, false);
+    return () => {
+      window.removeEventListener("resize", resize, false);
+      window.removeEventListener("orientationchange", resize, false);
+      window.removeEventListener("pointermove", pointermove, false);
+    };
+  }, []);
+
+  function pointermove(e: MouseEvent) {
+    // var mouse = transposeEvent(e);
+    // console.log(mouse.clientX, mouse.clientY);
+  }
+
+  function resize() {
+    if (window.navigator.maxTouchPoints > 0) {
+      document.oncontextmenu = () => false;
+    }
+    setIsMobile(window.navigator.maxTouchPoints > 0);
+    setWidth(getWidth());
+    setHeight(getHeight());
+  }
+
   return (
-    <div className="App">
-      hey
+    <div
+      style={{ width, height }}
+      className={["App", isMobile ? "mobile" : ""].join(" ")}
+    >
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
     </div>
   );
 }
-
-export default App;
